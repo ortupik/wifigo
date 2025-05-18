70d1862b337c26ea3f5c1b5f99eacb080a86c72c
chrisx <chrisadriane.ca@gmail.com>
2025-05-18 09:38:52 +0300

    fix mik

--- server/handler/mpesa.go
+++ server/handler/mpesa.go
@@ -52,6 +52,7 @@ type MpesaHandler struct {
 func LoadMpesaConfig() (*MpesaConfig, error) {
 	var config MpesaConfig
 	sub := nconfig.GetConfig().Sub("mpesa")
+	fmt.Printf("sub: %v\n", sub)
 	if sub == nil {
 		return nil, fmt.Errorf("mpesa config not found")
 	}
