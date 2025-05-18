3de3425a40055c7e190c88d1c94ad87b3c2542e9
chrisx <chrisadriane.ca@gmail.com>
2025-05-18 11:41:51 +0300

    test mikrotik

--- server/main.go
+++ server/main.go
@@ -100,7 +100,7 @@ func main() {
 
 	currentDevice, _ = mikrotikManager.GetDevice(sampleDevice.ID)
 
-	fmt.Println(currentDevice.Execute(`/interface print`))
+	fmt.Println(currentDevice.Execute("/interface print"))
 
 	wsHub := websocket.NewHub()
 	go wsHub.Run()
